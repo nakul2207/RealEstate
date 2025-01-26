2ccc1e16fa86f96f544636f323f1833827cdcdee
nakul gupta <ncvs@cvs.du.ac.in>
2025-01-26 23:46:20 +0530

    python server added

--- backend/server/server.py
+++ backend/server/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request,jsonify
 from flask_cors import CORS 
 import util
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -27,4 +28,4 @@ def predict_home_price():
 
 if __name__ == "__main__":
     print("Python server running")
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
